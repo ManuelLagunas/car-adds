f3b07d2341c0bb1df43e0c188d2c5586e1a39b6e
ManuelLagunas <lagunas.aragon@gmail.com>
2023-10-17 17:36:31 -0600

    [Funcionando antes d ela imagen]

--- app.py
+++ app.py
@@ -31,7 +31,11 @@ if table_button:
     tab = ff.create_table(df_sample)
     tab.show()
 
+
 #Botón para construir histogramas
+
+st.markdown('### Create an histogram based on the odometer column.')
+
 hist_button = st.button('Draw an histogram')
 
 if hist_button:
@@ -40,9 +44,29 @@ if hist_button:
     st.plotly_chart(fig, use_container_width=True)
 
 #Botón para construir un grafico de dispersión
+
+st.markdown('### Create a scatter graph based on dodometer vs price.')
+
 scatter_button = st.button('Draw a scatter graph')
 
 if scatter_button:
     st.write('Drawing a scatter graph for the car\'s add dataframe, base on the odometer vs price')
     fig_sct = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig_sct, use_container_width=True)
+
+#Crear contenido con checkboxes
+
+st.markdown('### Please select a checkbox. You decide how you want to see the information.')
+
+scatter_check = st.checkbox('Draw a scatter graph')
+histogram_check = st.checkbox('Draw an histogram')
+
+if scatter_check:
+    st.write('Drawing a scatter graph for the car\'s add dataframe, base on the year vs price')
+    check_sct = px.scatter(car_data, x='model_year', y='price')
+    st.plotly_chart(check_sct, use_container_width=True)
+
+if histogram_check:
+    st.write('Drawing an histogram for the car\'s add dataframe, base on the model year')
+    hist = px.histogram(car_data, x='model_year')
+    st.plotly_chart(hist, use_container_width=True)
